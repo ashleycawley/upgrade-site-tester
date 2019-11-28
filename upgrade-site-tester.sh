f29c3c26b78919acfd05ea181a1ca05c8868bd99
Ashley Cawley <ash@ashleycawley.co.uk>
2019-11-28 16:20:23 +0000

    Seems to be working nicely

--- upgrade-site-tester.sh
+++ upgrade-site-tester.sh
@@ -7,18 +7,19 @@
 # Variables
 SAVEIFS=$IFS	# Backing up the delimiter used by arrays to differentiate between different data in the array (prior to changing it)
 IFS=$'\n'	# Changing the delimiter used by arrays from a space to a new line, this allows a list of users (on new lines) to be stored in to an array
-NUMBER_OF_SITES="20"
+NUMBER_OF_SITES="30"
 RANDOM_SITES=$(cat /etc/localdomains | shuf -n $NUMBER_OF_SITES) # Variable needs to be below $NUMBER_OF_SITES
-
+CURL_TIMEOUT="5" # Number of seconds
 # Functions
 
 # Main Script
-cat /dev/null > /tmp/online-sites.log # Clears temporary log from any prior runs
+#cat /dev/null > /tmp/online-sites.log # Clears temporary log from any prior runs
+
 
 # Menu asking the user if this is the first run or post upgrade testing
-MENU_OPTION=$(whiptail --title "Upgrade Site Tester" --menu "Choose an option" 25 78 16 \
+MENU_OPTION=$(whiptail --title "Upgrade Site Tester" --menu "Choose an option" 20 60 5 \
 "First Run" "Gathers a list of working sites." \
-"Post-Upgrade Check" "Runs tests on the previously gathered list of sites that were working." 3>&1 1>&2 2>&3)
+"Post Upgrade Test" "Runs tests on previously working sites." 3>&1 1>&2 2>&3)
 
 case $MENU_OPTION in
     "First Run") FIRST_RUN="YES" ;;
@@ -30,24 +31,39 @@ then
     for SITE in $RANDOM_SITES
     do
         # Tests site
-        RESULT=$(curl -Is https://$SITE | grep 'HTTP/1.1 200 OK')
+        RESULT=$(curl --max-time $CURL_TIMEOUT -Is https://$SITE | grep 'HTTP/1.1 200 OK')
 
-        echo "$SITE"
-        echo "$RESULT"
-        echo
-        echo
-
-        [[ $RESULT =~ "200 OK" ]] && echo "$SITE" >> /tmp/online-sites.log
-
-
-
-        #  case $RESULT in
-        #      "HTTP/1.1 200 OK") echo "200 OK :-)" ;;
-        #      *) echo "Not OK ;----("
-        #  esac
+        # Logs working websites into a temporary log file
+        [[ $RESULT =~ "200 OK" ]] && echo "$SITE" >> /tmp/online-sites.log && echo "$SITE saved for later re-testing."
 
     done
+
+    IFS=$SAVEIFS # Resets $IFS this changes the delimiter that arrays use from new lines (\n) back to just spaces (which is what it normally is)
+
+    exit 0
+
+else
+
+    echo "POST UPGRADE MODE ENABLED..."
+
+    for SITE in $(cat /tmp/online-sites.log)
+    do
+        echo "Testing... $SITE"
+
+        # Tests site
+        RESULT=$(curl --max-time $CURL_TIMEOUT -Is https://$SITE | grep 'HTTP/1.1 200 OK')
+
+        [[ ! $RESULT =~ "200 OK" ]] && echo "The is a problem with $SITE" && echo "Result: `curl --max-time $CURL_TIMEOUT -Is https://$SITE | head -n 1`"
+
+        sleep 2
+
+    done
+
+    cat /dev/null > /tmp/online-sites.log # Clears temporary log from any prior runs
+
 fi
+
+
 
 IFS=$SAVEIFS # Resets $IFS this changes the delimiter that arrays use from new lines (\n) back to just spaces (which is what it normally is)
 
