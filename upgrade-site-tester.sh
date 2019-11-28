0296e5173efe045f1ac77583536f4021b857772d
Ashley Cawley <ash@ashleycawley.co.uk>
2019-11-28 14:22:34 +0000

    init commit

---
+++ upgrade-site-tester.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Notes: A script to test a variety of sites on a shared server before, during and
+#        after a software upgrade.
+#
+# Author: Ashley Cawley // ash@ashleycawley.co.uk // @ashleycawley
+
+# Variables
+NUMBER_OF_SITES="10"
+
+# Functions
+
+# Main Script
+
+# Exit
+exit 0
